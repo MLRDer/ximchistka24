2f3961b88315efdceb02cbefd231ba5e295cb26b
MLRDer <r.qirgizboyev@mail.ru>
2021-03-18 23:49:44 +0500

    test

--- app.js
+++ app.js
@@ -1,58 +1,67 @@
 // all needed packages
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-require('dotenv/config');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+require("dotenv/config");
 
 // routes
-const loginRoute = require('./routes/login');
-const welcomeRoute = require('./routes/welcome');
-const aboutRoute = require('./routes/about');
-const galleryRoute = require('./routes/gallery');
-const serviceRoute = require('./routes/service');
+const loginRoute = require("./routes/login");
+const welcomeRoute = require("./routes/welcome");
+const aboutRoute = require("./routes/about");
+const galleryRoute = require("./routes/gallery");
+const serviceRoute = require("./routes/service");
 
 // database connection
 const ConnectionString = process.env.ConnectionString;
-mongoose.connect(
-	ConnectionString,
-	{
-		useUnifiedTopology: true,
-		useNewUrlParser: true,
-		useFindAndModify: false,
-		useCreateIndex: true
-	}
-);
-
-
-// all middleware functions
-app.disable('x-powered-by');
-app.use(bodyParser.json({
-	limit: '50mb', extended: true
-}));
-app.use(bodyParser.urlencoded({
-	limit: "50mb", extended: true, parameterLimit: 50000
-}));
-
-app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	res.header("Access-Control-Expose-Headers", "x-token")
-	next();
+mongoose.connect(ConnectionString, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
 });
 
-app.use('/api/uploads', express.static('uploads'));
-app.use('/api/welcome', welcomeRoute);
-app.use('/api/about', aboutRoute);
-app.use('/api/login', loginRoute);
-app.use('/api/service', serviceRoute);
-app.use('/api/gallery', galleryRoute);
+// all middleware functions
+app.disable("x-powered-by");
+app.use(
+    bodyParser.json({
+        limit: "50mb",
+        extended: true,
+    })
+);
+app.use(
+    bodyParser.urlencoded({
+        limit: "50mb",
+        extended: true,
+        parameterLimit: 50000,
+    })
+);
 
-app.get('/', (req, res) => {
-	res.send('hello')
-})
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Expose-Headers", "x-token");
+    next();
+});
+
+app.use("/api/uploads", express.static("uploads"));
+app.use("/api/welcome", welcomeRoute);
+app.use("/api/about", aboutRoute);
+app.use("/api/login", loginRoute);
+app.use("/api/service", serviceRoute);
+app.use("/api/gallery", galleryRoute);
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 // listen
 app.listen(4000, () => {
-	console.log('Port listening...')
-})
+    console.log("Port listening...");
+});
---
+++ build/precache-manifest.7a59f08ef2373f3d82797323e5c092e5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e82389b358a45137841b9d9cf79b4e8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c489c64f028a4d79abe3",
+    "url": "/static/css/main.9a34c4f5.chunk.css"
+  },
+  {
+    "revision": "379f097a99c69456864b",
+    "url": "/static/js/2.bacf9e3d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.bacf9e3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c489c64f028a4d79abe3",
+    "url": "/static/js/main.6839daaa.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "06602283c24ede2301e4b887e6b9b051",
+    "url": "/static/media/logo1.06602283.jpg"
+  },
+  {
+    "revision": "1f5abe2cba6b7e4826c7252630b2a036",
+    "url": "/static/media/service1.1f5abe2c.jpg"
+  },
+  {
+    "revision": "cd7c042f9d53092c080029c4dfa61081",
+    "url": "/static/media/service2.cd7c042f.jpg"
+  },
+  {
+    "revision": "7808346b438952545a2495659e54500b",
+    "url": "/static/media/service3.7808346b.jpg"
+  },
+  {
+    "revision": "f5a49fd800c770f22102902933463b5c",
+    "url": "/static/media/service4.f5a49fd8.jpg"
+  },
+  {
+    "revision": "c93f4e4ef30100513adce7b964b3f132",
+    "url": "/static/media/service6.c93f4e4e.jpg"
+  },
+  {
+    "revision": "150a06f058221ff2cd3868639d72e3c0",
+    "url": "/static/media/service7.150a06f0.jpg"
+  },
+  {
+    "revision": "8e39de6f9e30406a02311a1c5053f171",
+    "url": "/static/media/service9.8e39de6f.jpg"
+  },
+  {
+    "revision": "7ac566db9067f835c3cc685eb3c7c17f",
+    "url": "/static/media/slide1.7ac566db.jpeg"
+  },
+  {
+    "revision": "c684108531998d3f04c26bf9e92a0707",
+    "url": "/static/media/slide2.c6841085.jpg"
+  },
+  {
+    "revision": "6e93f069fd5b2d15489f7e934fcc0e4a",
+    "url": "/static/media/slide3.6e93f069.jpg"
+  }
+]);
